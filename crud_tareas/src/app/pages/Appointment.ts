80babdc2c39f7cb230e9ed48d55beafe478f3e30
dennis <dennis.nunez@epn.edu.ec>
2020-01-24 16:42:45 -0500

    proyecto

---
+++ crud_tareas/src/app/pages/Appointment.ts
@@ -0,0 +1,5 @@
+export class Appointment {
+    $key: string;
+    activity: string;
+    priority: string;
+}
---
+++ crud_tareas/src/app/shared/appointment.service.ts
@@ -0,0 +1,48 @@
+import { Injectable } from '@angular/core';
+import { Appointment } from '../pages/Appointment';
+import { AngularFireDatabase, AngularFireList, AngularFireObject } from '@angular/fire/database';
+
+@Injectable({
+  providedIn: 'root'
+})
+
+export class AppointmentService {
+  bookingListRef: AngularFireList<any>;
+  bookingRef: AngularFireObject<any>;
+
+  constructor(private db: AngularFireDatabase) { }
+
+  // Create
+  createBooking(apt: Appointment) {
+    return this.bookingListRef.push({
+      name: apt.activity,
+      email: apt.priority
+    })
+  }
+
+  // Get Single
+  getBooking(id: string) {
+    this.bookingRef = this.db.object('/appointment/' + id);
+    return this.bookingRef;
+  }
+
+  // Get List
+  getBookingList() {
+    this.bookingListRef = this.db.list('/appointment');
+    return this.bookingListRef;
+  }
+
+  // Update
+  updateBooking(id, apt: Appointment) {
+    return this.bookingRef.update({
+      name: apt.activity,
+      email: apt.priority
+    })
+  }
+
+  // Delete
+  deleteBooking(id: string) {
+    this.bookingRef = this.db.object('/appointment/' + id);
+    this.bookingRef.remove();
+  }
+}
